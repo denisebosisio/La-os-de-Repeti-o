160580a5a454bfb0f6782f4fd2cb87b71dbc9c9c
denisebosisio <denisebosisio12@gmail.com>
2022-06-14 19:30:09 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("Contando de 1 até 10")
+for(let i=1; i<=10; i++){
+    console.log(i);
+}
+
+console.log("Contando de 10 até 1")
+let num = 10;
+while(num >= 1){
+    console.log(num);
+    num--;
+}
+
+console.log("Contando de 1 até 100")
+let contImpar = 1
+while(contImpar <= 100){
+    console.log(contImpar);
+    contImpar++;
+}
+
+console.log("Contando de 0 até 100")
+for(let i=0; i<=100; i++){
+    console.log(i);
+}
